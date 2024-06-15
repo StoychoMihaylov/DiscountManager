3d99ee619ffaaf52b161a106cfa2cae478f1d0d0
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2024-06-16 01:50:44 +0200

    mongo context and discount code document added

---
+++ DiscountManager.Data/Context/MongoDbContext.cs
@@ -0,0 +1,32 @@
+﻿namespace DiscountManager.Data.Context
+{
+    using DiscountManager.Data.Documents;
+    using DiscountManager.Models.Options;
+    using Microsoft.Extensions.Options;
+    using MongoDB.Driver;
+
+    public class MongoDbContext
+    {
+        private readonly MongoClient _client;
+        private readonly IMongoDatabase _database;
+
+        public MongoDbContext(IOptions<MongoDbOptions> dbOptions)
+        {
+            var settings = dbOptions.Value;
+            _client = new MongoClient(settings.ConnectionString);
+            _database = _client.GetDatabase(settings.DatabaseName);
+            CreateIndex();
+        }
+
+        public IMongoClient Client => _client;
+        public IMongoDatabase Database => _database;
+
+        private void CreateIndex()
+        {
+            var codeCollection = _database.GetCollection<DiscountCodeDocument>(nameof(DiscountCodeDocument));
+            var indexKeysDefinition = Builders<DiscountCodeDocument>.IndexKeys.Ascending(code => code.Code);
+            var indexOptions = new CreateIndexOptions { Unique = true };
+            codeCollection.Indexes.CreateOne(new CreateIndexModel<DiscountCodeDocument>(indexKeysDefinition, indexOptions));
+        }
+    }
+}
